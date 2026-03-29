1574f374164752e26405b3719a362a56ebce49a1
Asif Raza <syedasifrazaofficial@gmail.com>
2026-03-29 14:30:08 +0500

    Next.JS Application

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import { ReactNode } from "react";
+
+import "./globals.css";
+
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
+});
+
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+      </head>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+        >
+            
+
+            {children}
+
+        </body>
+   
+    </html>
+  );
+};
+
+export default RootLayout;
